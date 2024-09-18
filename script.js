6c92ad52dc0adce9e6d2948470e6dc72d613da5a
Sunil Sapkota <suneelsapkota@gmail.com>
2024-09-18 09:44:01 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// let spanHi= document.getElementById("hi")
+// spanHi.style.backgroundColor="red";
+// spanHi.style.display="none"
+
+// let first = document.getElementsByClassName("first");
+// console.log("first", first);
+// first.style.backgroundColor = "red";
+
+let hi = document.getElementById("hi");
+console.log(hi.dataset.test,"this is dataset")
+
+
