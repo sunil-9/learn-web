2ea5ddd7aab9b2c00cc32e8a74653bab681b74e1
Sunil Sapkota <suneelsapkota@gmail.com>
2024-09-22 09:40:52 +0545

    Refactor counter functionality and add color change feature

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let addBtn = document.getElementById("btnAdd");
 let minusBtn = document.getElementById("btnMinus");
 let counter = 0;
+let colors = ["red", "green", "blue"];
 
 addBtn.onclick = function () {
   //   console.log("add btn clicked");
   counter++;
   let ourText = document.getElementById("ourText");
   ourText.innerText = counter;
+  ourText.style.color = colors[counter % 3];
 };
 minusBtn.onclick = function () {
-    counter--;
-    let ourText = document.getElementById("ourText");
-    ourText.innerText = counter;
+  if (counter == 0) return;
+  counter--;
+  let ourText = document.getElementById("ourText");
+  ourText.innerText = counter;
+  ourText.style.color = colors[counter % 3];
 
 };
